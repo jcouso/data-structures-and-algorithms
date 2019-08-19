2a35f0c658ceb0963a4456b58e95a8b4e8359bf5
Juan Couso <juan.couso@gmail.com>
2019-08-19 14:20:12 -0300

    adding work

---
+++ data_structures/linked_list/linked_list.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printList();
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+int main () {
+  struct Node* head = NULL;
+  struct Node* second = NULL;
+  struct Node* third = NULL;
+
+
+  
+  head = (struct Node*)malloc(sizeof(struct Node));
+  second = (struct Node*)malloc(sizeof(struct Node));
+  third = (struct Node*)malloc(sizeof(struct Node));
+  
+  head->data = 1;
+  head->next = second;
+
+  second->data = 2;
+  second->next = third;
+
+  third->data = 3;
+  second->next = NULL;
+
+  printList(head);
+
+}
+ 
+
+void printList(struct Node *n)
+{
+  while (n != NULL)
+  {
+     printf(" %d ", n->data);
+     n = n->next;
+  }
+}
